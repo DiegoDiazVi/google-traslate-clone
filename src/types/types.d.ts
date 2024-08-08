8c016bd0d6933210b17467920780c10640a90c77
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 15:14:47 -0500

    feat: Update language translation types and interfaces

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,22 +1,27 @@
+import { AUTO_LANGUAGE, SUPPORTED_LANGUAGES } from '../constants/constants';
 export interface StateLanguages {
     fromLanguage: string;
-    toLanguage: string;
-    fromText: string;
-    resultText: string;
-    loading: boolean;
+    toLanguage:   string;
+    fromText:     string;
+    resultText:   string;
+    loading:      boolean;
 }
 
 export type ActionReducer =
     | {type: 'INTERCHANGE_LANGUAGES'}
-    | {type: 'SET_FROM_LANGUAGE', payload: string}
-    | {type: 'SET_TO_LANGUAGE', payload: string}
-    | {type: 'SET_FROM_TEXT_LANGUAGE', payload: string}
+    | {type: 'SET_FROM_LANGUAGE',        payload: string}
+    | {type: 'SET_TO_LANGUAGE',          payload: string}
+    | {type: 'SET_FROM_TEXT_LANGUAGE',   payload: string}
     | {type: 'SET_RESULT_TEXT_LANGUAGE', payload: string}
 
 interface UseLanguageStateType extends StateLanguages {
     interchangeLanguages:   () => void,
-    setFromLanguages:       (fromLanguage: string) => void,
-    setToLanguages:         (toLanguage: string) => void,
+    setFromLanguages:       (fromLanguage: Language) => void,
+    setToLanguages:         (toLanguage: Language) => void,
     setFromTextLanguages:   (fromText: string) => void,
     setResultTextLanguages: (resultText: string) => void
 }
+
+export type Language     = keyof typeof SUPPORTED_LANGUAGES;
+export type AutoLanguage = typeof AUTO_LANGUAGE;
+export type FromLanguage = Language | AutoLanguage;
