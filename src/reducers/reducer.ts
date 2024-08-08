05c5e1c536b73b92ba31537dec23f6d79858d847
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 13:13:19 -0500

    feat: Add reducer for language translation

---
+++ src/reducers/reducer.ts
@@ -0,0 +1,39 @@
+import type{ ActionReducer, StateLanguages } from "../types/types.d";
+
+export function reducer(state: StateLanguages, action: ActionReducer): StateLanguages {
+    const { type } = action;
+
+    switch ( type ) {
+        case 'INTERCHANGE_LANGUAGES':
+            return {
+                ...state,
+                fromLanguage: state.toLanguage,
+                toLanguage: state.fromLanguage,
+            }
+        case 'SET_FROM_LANGUAGE':
+            return {
+                ...state,
+                fromLanguage: action.payload
+            }
+        case 'SET_TO_LANGUAGE':
+            return {
+                ...state,
+                toLanguage: action.payload
+            }
+        case 'SET_FROM_TEXT_LANGUAGE':
+            return {
+                ...state,
+                loading: true,
+                fromText: action.payload,
+                resultText: ''
+            }
+        case 'SET_RESULT_TEXT_LANGUAGE':
+            return {
+                ...state,
+                loading: false,
+                resultText: action.payload
+            }
+        default:
+            throw new Error(`Action type "${type}" is not defined`);
+    }
+}
