ec573d1b11d1357194f854c8f586fa4208c9f490
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 14:54:51 -0500

    feat: Add UseLanguageStateType interface for language translation management

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -12,3 +12,11 @@ export type ActionReducer =
     | {type: 'SET_TO_LANGUAGE', payload: string}
     | {type: 'SET_FROM_TEXT_LANGUAGE', payload: string}
     | {type: 'SET_RESULT_TEXT_LANGUAGE', payload: string}
+
+interface UseLanguageStateType extends StateLanguages {
+    interchangeLanguages:   () => void,
+    setFromLanguages:       (fromLanguage: string) => void,
+    setToLanguages:         (toLanguage: string) => void,
+    setFromTextLanguages:   (fromText: string) => void,
+    setResultTextLanguages: (resultText: string) => void
+}
