b5cd4482d8939589040fc61b066e1756923b5f10
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 14:32:23 -0500

    chore: Remove unused code and dependencies in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { useReducer } from 'react';
-import { reducer } from './reducers/reducer';
-import { initialState } from './mocks/initialState';
+
 
 function App(): JSX.Element {
-  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       <h1>Google Traslate</h1>
