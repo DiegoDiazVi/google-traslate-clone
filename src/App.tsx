1b136e31a4a32a2de561f9d6210a7e47d77d4459
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 12:28:22 -0500

    feat: add React Boostrap to get styles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1>Google Traslate</h1>
+    </div>
+  );
+}
+
+export default App;
