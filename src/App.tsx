4d63c59bf75cf421930a575aaf12a7422a83c9b1
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 15:36:04 -0500

    feat: Update App component to include language selection and interchange functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import useLanguageState from './hooks/useLanguageState.ts';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Col, Row } from 'react-bootstrap';
 import './App.css';
 
 
+
 function App(): JSX.Element {
+  const { fromLanguage, toLanguage, setFromLanguages, interchangeLanguages } = useLanguageState()
   return (
-    <div className="App">
+    <Container fluid>
       <h1>Google Traslate</h1>
-    </div>
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <button onClick={() => interchangeLanguages()}>Interchange</button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
