7e514ee6404ef877bcb8ea00aecaf9affadc9ab2
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 14:55:33 -0500

    feat: return dispatch functions to manage state

--- src/hooks/useLanguageState.ts
+++ src/hooks/useLanguageState.ts
@@ -1,10 +1,59 @@
 import { useReducer } from 'react';
 import { reducer } from '../reducers/reducer';
 import { initialState } from '../mocks/initialState';
-function useLanguageState() {
+import type{ UseLanguageStateType } from '../types/types.d';
+
+
+function useLanguageState(): UseLanguageStateType {
     const [ state, dispatch ] = useReducer(reducer, initialState);
     const { fromLanguage, toLanguage, fromText, resultText, loading } = state;
 
+    const interchangeLanguages = (): void => {
+        dispatch({
+            type: 'INTERCHANGE_LANGUAGES'
+        })
+    }
+
+    const setFromLanguages = (fromLanguage: string): void => {
+        dispatch({
+            type: 'SET_FROM_LANGUAGE',
+            payload: fromLanguage
+        })
+    }
+
+    const setToLanguages = (toLanguage: string): void => {
+        dispatch({
+            type: 'SET_TO_LANGUAGE',
+            payload: toLanguage
+        })
+    }
+
+    const setFromTextLanguages = (fromText: string): void => {
+        dispatch({
+            type: 'SET_FROM_TEXT_LANGUAGE',
+            payload: fromText
+        })
+    }
+
+    const setResultTextLanguages = (resultText: string): void => {
+        dispatch({
+            type: 'SET_RESULT_TEXT_LANGUAGE',
+            payload: resultText
+        })
+    }
+
+    return {
+        fromLanguage,
+        toLanguage,
+        fromText,
+        resultText,
+        loading,
+        interchangeLanguages,
+        setFromLanguages,
+        setToLanguages,
+        setFromTextLanguages,
+        setResultTextLanguages
+    }
 }
 
 export default useLanguageState;
