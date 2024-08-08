69de944fe874d12cf15ccca4fd288901e586851a
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 13:13:29 -0500

    feat: Add types for language translation state

---
+++ src/types/types.d.ts
@@ -0,0 +1,14 @@
+export interface StateLanguages {
+    fromLanguage: string;
+    toLanguage: string;
+    fromText: string;
+    resultText: string;
+    loading: boolean;
+}
+
+export type ActionReducer =
+    | {type: 'INTERCHANGE_LANGUAGES'}
+    | {type: 'SET_FROM_LANGUAGE', payload: string}
+    | {type: 'SET_TO_LANGUAGE', payload: string}
+    | {type: 'SET_FROM_TEXT_LANGUAGE', payload: string}
+    | {type: 'SET_RESULT_TEXT_LANGUAGE', payload: string}
