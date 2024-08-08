bd735506a071cc7f7bb8a573121aabc439d29365
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 13:13:43 -0500

    feat: Add useReducer hook and initial state for language translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { useReducer } from 'react';
+import { reducer } from './reducers/reducer';
+import { initialState } from './mocks/initialState';
 
 function App(): JSX.Element {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       <h1>Google Traslate</h1>
