4456506e0a306ae59f9b9afb46d0ad0041adde7e
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-08-08 16:45:11 -0500

    feat: Update LanguageSelector component to include auto language detection option

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,18 +1,21 @@
 import { Form } from "react-bootstrap";
-import { SUPPORTED_LANGUAGES } from "../constants/constants";
+import { AUTO_LANGUAGE, SUPPORTED_LANGUAGES } from "../constants/constants";
 import type { FromLanguage, Language } from '../types/types';
 
 type Props =
     | { type: 'from', value: FromLanguage, onChange: (payload: FromLanguage) => void }
     | { type: 'to',   value: Language,     onChange: (payload: Language) => void }
 
-function LanguageSelector({ onChange }: Props): JSX.Element {
+function LanguageSelector({ onChange, type, value}: Props): JSX.Element {
 
     const handlerChange = (evt: React.ChangeEvent<HTMLSelectElement>): void => {
         onChange(evt.target.value as Language)
     }
     return (
-        <Form.Select aria-label='Selecciona el idioma' onChange={handlerChange}>
+        <Form.Select aria-label='Selecciona el idioma' onChange={handlerChange} value={value}>
+
+            { type === 'from' && <option value={AUTO_LANGUAGE}>Detectar idioma</option>}
+
             {Object.entries(SUPPORTED_LANGUAGES).map(([key, value]) => {
                 return (
                     <option key={key} value={key}>
